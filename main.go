6ccd83e5dce3b6321785d4c29d10922ec799a187
tike <timflex@gmx.de>
2018-08-18 20:25:56 +0200

    cleanup main

--- main.go
+++ main.go
@@ -8,25 +8,42 @@ import (
 	"github.com/tike/roadrunner/internal/scan"
 )
 
+// flags
+var (
+	verbose bool
+)
+
 func main() {
-	flag.Usage = Usage
-	flag.Parse()
-	if flag.NArg() != 1 {
-		flag.Usage()
-		os.Exit(127)
-	}
+	flagFoo()
+
 	fmt.Println("package to watch:", flag.Arg(0))
-	packs, err := scan.GetPkgs(flag.Arg(0))
+	pkgs, err := scan.GetPkgs(flag.Arg(0))
 	if err != nil {
 		fmt.Println("couldn't find package", err)
 		os.Exit(1)
 	}
-	fmt.Println("watching dependecies:")
+	if verbose {
+		printPackageList(pkgs)
+	}
+}
+
+func printPackageList(packs scan.PkgPack) {
+	fmt.Println("watching packages:")
 	f := "%-20s %-60s %s\n"
 	fmt.Printf(f, "name", "import", "src")
 	for _, imp := range packs.SortedNames() {
 		pkg := packs[imp]
 		fmt.Printf(f, pkg.Name, imp, pkg.ImportPath)
+	}
+}
+
+func flagFoo() {
+	flag.Usage = Usage
+	flag.BoolVar(&verbose, "v", true, "verbose output")
+	flag.Parse()
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(127)
 	}
 }
 

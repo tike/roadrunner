4a06493f8918e787e815cf70dac616dcf820c56c
tike <timflex@gmx.de>
2018-08-18 20:18:25 +0200

    filelist

--- internal/scan/pkgPack.go
+++ internal/scan/pkgPack.go
@@ -2,6 +2,7 @@ package scan
 
 import (
 	"go/build"
+	"path/filepath"
 	"sort"
 )
 
@@ -11,6 +12,18 @@ func (p pkgPack) SortedNames() []string {
 	var names = make(sort.StringSlice, 0, len(p))
 	for name := range p {
 		names = append(names, name)
+	}
+
+	names.Sort()
+	return names
+}
+
+func (p pkgPack) FileList() []string {
+	var names = make(sort.StringSlice, 0, 5*len(p))
+	for _, pkg := range p {
+		for _, file := range pkg.GoFiles {
+			names = append(names, filepath.Join(pkg.ImportPath, file))
+		}
 	}
 
 	names.Sort()
